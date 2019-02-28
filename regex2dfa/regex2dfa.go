59d3f11ece5b229626cd729d9deebafbc1872a17
Cecylia Bocovich <cohosh@torproject.org>
2019-02-28 11:01:28 -0500

    Reordered the LDFLAGS in the cgo directive for regex2dfa to avoid a linker error since libfst is dependent on dl

--- regex2dfa/regex2dfa.go
+++ regex2dfa/regex2dfa.go
@@ -1,7 +1,7 @@
 package regex2dfa
 
 // #cgo CXXFLAGS: -std=c++11 -DMARIONETTE -I${SRCDIR}/../third_party/re2/ -I${SRCDIR}/../third_party/openfst/src/include/
-// #cgo LDFLAGS: -ldl ${SRCDIR}/../third_party/libs/libfst.a ${SRCDIR}/../third_party/libs/libfstscript.a ${SRCDIR}/../third_party/libs/libre2.a
+// #cgo LDFLAGS: ${SRCDIR}/../third_party/libs/libfst.a ${SRCDIR}/../third_party/libs/libfstscript.a ${SRCDIR}/../third_party/libs/libre2.a -ldl
 // #include <stdlib.h>
 // #include <stdint.h>
 // int _regex2dfa(const char* input_regex, uint32_t input_regex_len, char **out, size_t *sz);
